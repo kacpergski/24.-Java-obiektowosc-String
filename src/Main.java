2a7b392ba9e1a6611f6fc2a962382dcd29428286
Kacper Grzybkowski <kacperg@gmail.com>
2021-01-24 23:43:40 +0100

    #3 Recipies mod1

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,23 @@ public class Main {
                     System.out.println("Postcode " +postCode + " inncorect");
                 }
             }
+
+            //Zadanie 2
+        System.out.println();
+        System.out.println("*******************************************************");
+        System.out.println();
+        RecipeRepo recipiesAll = new RecipeRepo(5);
+
+        String [] ingredients = {"woda","mąka","drożdże"};
+
+        Recipe r1 = new Recipe("Chleb", ingredients, 20,"STEP 1 Mix 500g strong white flour, 2 tsp salt and a 7g sachet of fast-action yeast in a large bowl. STEP 2  Make a well in the centre, then add 3 tbsp olive oil and 300ml water, and mix well. If the dough seems a little stiff, add another 1-2 tbsp water and mix. STEP 3 Tip onto a lightly floured work surface and knead for around 10 mins. STEP 4 Once the dough is satin-smooth, place it in a lightly oiled bowl and cover with cling film. Leave to rise for 1 hour until doubled in size or place in the fridge overnight. STEP 5 Line a baking tray with baking parchment. Knock back the dough (punch the air out and pull the dough in on itself) then gently mould the dough into a ball.");
+        recipiesAll.add(r1);
+        Recipe r2 = new Recipe("Chleb2", ingredients, 25,"Zmieszać i upiec");
+        recipiesAll.add(r2);
+        //r1.displayOne();
+
+
+        recipiesAll.displayAll(1);
     }
 
 
